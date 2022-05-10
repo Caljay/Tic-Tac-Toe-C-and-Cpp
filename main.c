41eacbea393eabf7b46c835989535e8ccf61044f
Caljay <99057688+Caljay@users.noreply.github.com>
2022-05-09 20:50:28 -0700

    modified:   main.c 	modified:   main.exe 	.vscode/settings.json

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+char* welcome(void){
+
+    char* p_playerName = malloc(16);
+    system("cls");
+    
+    printf("Welcome to Story Game!\n");
+    printf("What is your name? ");
+    scanf("%s", p_playerName);
+
+    printf("Welcome %s, We hope you survive...", p_playerName);
+    return p_playerName;
+
+
+
+
+}
+
+
+
 
 int main(void){
-    printf("Hello Story Game");
+    printf("Hello Story");
+char* p_playerName = welcome();
+
+
+
+
+
+
+
+
     return 0;
 }
