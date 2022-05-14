dd6ea1d91c8324235483bd9711e46162143f1e78
unknown <caljay@wayjay.com>
2022-05-14 13:48:52 -0700

    modified:   main.c 	modified:   main.exe 	.vscode/settings.json

--- main.c
+++ main.c
@@ -1,37 +1,56 @@
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 
-char* welcome(void){
+void loadBoard(short board[3]){
 
-    char* p_playerName = malloc(16);
-    system("cls");
-    
-    printf("Welcome to Story Game!\n");
-    printf("What is your name? ");
-    scanf("%s", p_playerName);
-
-    printf("Welcome %s, We hope you survive...", p_playerName);
-    return p_playerName;
+{
 
 
+        for (int j = 0; j < 3; j++){
+        
+        if(board[j] == 0)
+              printf("- ");
 
+        
+        }
+
+        printf("\n");
+       
+
+
+    }
 
 }
 
+void intiate(short top[], short middle[],short bottom[]){
 
+ for(int i = 0; i < 3; i++){
+
+        top[i] = 0;
+        middle[i] = 0;
+        bottom[i] = 0;
+    }
+
+printf("\n\nWelcome to Tic Tac Toe\nHere is your board:\n");
+loadBoard(top);
+loadBoard(middle);
+loadBoard(bottom);
+}
 
 
 int main(void){
-    printf("Hello Story");
-char* p_playerName = welcome();
+    short top[3];
+    short middle[3];
+    short bottom[3];
+    
+   intiate(top, middle, bottom);
+
+int input = 0;
+printf("\nEnter a position!\nTop left is 1, bottom right is 9\n");
+scanf("%d",&input);
+printf("\n%d", input);
+
+    
 
 
-
-
-
-
-
-
-    return 0;
 }
